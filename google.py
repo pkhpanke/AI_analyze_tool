f52d9eed580cf88aaad6c0278c9e1931b6c5a2af
panke <2766466225@qq.com>
2025-01-03 16:18:29 +0800

    删除google.py

--- google.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-GEMINI_API_KEY = "AIzaSyAFN7Jn5lLXgeXPH0H7jc8CX63QGsMrzoE"
-genai.configure(api_key=GEMINI_API_KEY)
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
